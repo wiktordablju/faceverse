cef7b1c15e22ee019c889a59413c399fd260db7c
wiktordablju <wiktordablju@gmail.com>
2023-11-27 09:54:39 +0100

    27.11: rejestracja i logowanie

--- faceverse/faceverse/urls.py
+++ faceverse/faceverse/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('homepage.urls')),
     path('admin/', admin.site.urls),
     path('registration/', include('registration.urls')),
 ]
