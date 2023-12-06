5fa493173929ca81eace5cc093586d21c34785e7
wiktordablju <wiktordablju@gmail.com>
2023-12-06 18:07:56 +0100

    06.12: tworzenie grupy nic wiecej

--- group_management/views.py
+++ group_management/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import GroupForm
+from .models import Group
 
 
-@login_required(login_url='core:welcome')
+@login_required
 def groups(request):
-    return render(request, 'group_management/groups.html')
+    if request.method == 'POST':
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            group = form.save(commit=False)
+            group.save()
+            group.members.add(request.user)
+            return redirect('group_management:groups')
+    else:
+        form = GroupForm()
+    return render(request, 'group_management/groups.html', {'form': form})
